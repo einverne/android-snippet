eedd0d124c6555f4b356b86563eca625da4b440e
wujiawei <jiawei.wu@ihandysoft.com>
2016-08-18 15:00:07 +0800

    Let ScreenFaceDetector call FaceUp and FaceDown only once

--- app/src/main/java/info/einverne/exercise100/activity/HeadsUpActivity.java
+++ app/src/main/java/info/einverne/exercise100/activity/HeadsUpActivity.java
@@ -2,10 +2,9 @@ package info.einverne.exercise100.activity;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import info.einverne.exercise100.R;
@@ -13,13 +12,13 @@ import info.einverne.exercise100.detector.ScreenFaceDetector;
 import info.einverne.exercise100.detector.ShakeDetector;
 import timber.log.Timber;
 
-public class HeadsUpActivity extends AppCompatActivity implements SensorEventListener,
-        ShakeDetector.Listener, ScreenFaceDetector.Listener{
+public class HeadsUpActivity extends AppCompatActivity implements
+        ShakeDetector.Listener, ScreenFaceDetector.Listener {
 
     private TextView showFaceFlip;
     private ShakeDetector shakeDetector;
+    private ScreenFaceDetector screenFaceDetector;
     private SensorManager sensorManager;
-    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,62 +28,48 @@ public class HeadsUpActivity extends AppCompatActivity implements SensorEventLis
         showFaceFlip = (TextView) findViewById(R.id.showFaceFlip);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_GAME);
+        initSensor();
+    }
 
-        shakeDetector = new ShakeDetector(this);
-        shakeDetector.setSensitivity(ShakeDetector.SENSITIVITY_LIGHT);
-        shakeDetector.start(sensorManager);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initSensor();
+    }
 
-        ScreenFaceDetector screenFaceDetector = new ScreenFaceDetector(this);
-        screenFaceDetector.start(sensorManager);
+    private void initSensor() {
+        if (shakeDetector == null) {
+            shakeDetector = new ShakeDetector(this);
+            shakeDetector.setSensitivity(ShakeDetector.SENSITIVITY_LIGHT);
+            shakeDetector.start(sensorManager);
+        }
+
+        if (screenFaceDetector == null) {
+            screenFaceDetector = new ScreenFaceDetector(this);
+            screenFaceDetector.start(sensorManager);
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopSensor();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        shakeDetector.stop();
-        sensorManager.unregisterListener(this);
+        stopSensor();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
-            float ax = event.values[0];
-            Timber.d("x value: " + ax);
-            float ay = event.values[1];
-            Timber.d("y value: " + ay);
-            float az = event.values[2];
-            Timber.d("z value: " + az);
-//            float gz = event.values[2];
-//            if (mGZ == 0) {
-//                mGZ = gz;
-//            } else {
-//                if ((mGZ * gz) < 0) {
-//                    mEventCountSinceGZChanged++;
-//                    if (mEventCountSinceGZChanged == MAX_COUNT_GZ_CHANGE) {
-//                        mGZ = gz;
-//                        mEventCountSinceGZChanged = 0;
-//                        if (gz > 0) {
-//                            Log.d(TAG, "now screen is facing up.");
-//                        } else if (gz < 0) {
-//                            Log.d(TAG, "now screen is facing down.");
-//                        }
-//                    }
-//                } else {
-//                    if (mEventCountSinceGZChanged > 0) {
-//                        mGZ = gz;
-//                        mEventCountSinceGZChanged = 0;
-//                    }
-//                }
-//            }
+    private void stopSensor() {
+        if (shakeDetector != null) {
+            shakeDetector.stop();
         }
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        if (screenFaceDetector != null) {
+            screenFaceDetector.stop();
+        }
     }
 
     @Override
@@ -98,7 +83,8 @@ public class HeadsUpActivity extends AppCompatActivity implements SensorEventLis
     }
 
     @Override
-    public void FaceBottom() {
-
+    public void FaceDown() {
+        Timber.d("Face Down");
     }
+
 }
--- app/src/main/java/info/einverne/exercise100/detector/ScreenFaceDetector.java
+++ app/src/main/java/info/einverne/exercise100/detector/ScreenFaceDetector.java
@@ -19,19 +19,23 @@ public class ScreenFaceDetector implements SensorEventListener {
 
     private int accelerationThreshold = DETAULT_ACCELERATION_THRESHOLD;
 
+    private int screenOrientationThreshold = 60;
+    private int screenOrientationHighThreshold = 180 - screenOrientationThreshold;
+
     /**
      * Listener for screen face
      */
     public interface Listener {
         void FaceUp();
 
-        void FaceBottom();
+        void FaceDown();
     }
 
     private final Listener listener;
     private final SampleQueue queue = new SampleQueue();
     private SensorManager sensorManager;
     private Sensor accelerometer;
+    private Sensor magnetometer;
 
     public ScreenFaceDetector(Listener listener) {
         this.listener = listener;
@@ -43,45 +47,245 @@ public class ScreenFaceDetector implements SensorEventListener {
         }
 
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer != null) {
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (accelerometer != null && magnetometer != null) {
             this.sensorManager = sensorManager;
             sensorManager.registerListener(this, accelerometer,
                     SensorManager.SENSOR_DELAY_GAME);
+            sensorManager.registerListener(this, magnetometer,
+                    SensorManager.SENSOR_DELAY_GAME);
         }
-        return accelerometer != null;
+        return accelerometer != null && magnetometer != null;
     }
 
     public void stop() {
-        if (accelerometer != null) {
+        if (accelerometer != null && magnetometer != null) {
             sensorManager.unregisterListener(this, accelerometer);
+            sensorManager.unregisterListener(this, magnetometer);
             sensorManager = null;
             accelerometer = null;
+            magnetometer = null;
         }
+    }
+
+    float[] inclineGravity = new float[3];
+    float[] gravityValue;
+    float[] geomagneticValue;
+    float orientation[] = new float[3];
+    float azimuth;
+    float pitch;
+    float roll;
+    private ScreenFaceOrientation screenFaceOrientation = ScreenFaceOrientation.FaceVertical;
+    private ScreenFaceOrientation lastScreenFaceOrientation = ScreenFaceOrientation.FaceVertical;
+
+    public enum ScreenFaceOrientation {
+        FaceUp,
+        FaceDown,
+        FaceVertical
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         boolean faceChanged = isScreenFaceChange(event);
-        long timestmp = event.timestamp;
-        queue.add(timestmp, faceChanged);
+        long timestamp = event.timestamp;
+        queue.add(timestamp, faceChanged);
         if (queue.isFaceChanging()) {
+            changeScreenStatus(lastScreenFaceOrientation);
             queue.clear();
-            listener.FaceUp();
         }
     }
 
     private boolean isScreenFaceChange(SensorEvent event) {
-        float az = event.values[2];
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            gravityValue = event.values;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagneticValue = event.values;
+        }
+        if (gravityValue != null && geomagneticValue != null) {
+            float[] R = new float[9];
+            float[] I = new float[9];
 
-        final double magnitudeSquared = az * az;
-        return magnitudeSquared > accelerationThreshold * accelerationThreshold;
+            boolean success = SensorManager.getRotationMatrix(R, I, gravityValue, geomagneticValue);
+            if (success) {
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+                roll = (float) Math.toDegrees(orientation[2]);
+                final double magnitudeSquared = roll * roll;
+                if (magnitudeSquared < screenOrientationThreshold * screenOrientationThreshold) {
+                    lastScreenFaceOrientation = ScreenFaceOrientation.FaceUp;
+                } else if (magnitudeSquared > screenOrientationHighThreshold * screenOrientationHighThreshold) {
+                    lastScreenFaceOrientation = ScreenFaceOrientation.FaceDown;
+                } else if (magnitudeSquared > 80 * 80 && magnitudeSquared < 100 * 100) {
+                    screenFaceOrientation = ScreenFaceOrientation.FaceVertical;
+                }
+                return magnitudeSquared > screenOrientationHighThreshold * screenOrientationHighThreshold || magnitudeSquared < screenOrientationThreshold * screenOrientationThreshold;
+            }
+        }
+        return false;
     }
 
+    /**
+     * only this function change screenFaceOrientation
+     * If new Orientation is different from screenFaceOrientation, then change it
+     * otherwise, do nothing
+     * @param newOrientation new Orientation need to assign to screenFaceOrientation
+     */
+    private void changeScreenStatus(ScreenFaceOrientation newOrientation) {
+        if (screenFaceOrientation != newOrientation) {
+            screenFaceOrientation = newOrientation;
+            switch (screenFaceOrientation) {
+                case FaceUp:
+                    listener.FaceUp();
+                    break;
+                case FaceDown:
+                    listener.FaceDown();
+                    break;
+            }
+        }
+    }
 
-    /** Queue of samples. Keeps a running average. */
+    public boolean isTiltUpward() {
+        if (gravityValue != null && geomagneticValue != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+
+            boolean success = SensorManager.getRotationMatrix(R, I, gravityValue, geomagneticValue);
+
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+                /*
+                * If the roll is positive, you're in reverse landscape (landscape right), and if the roll is negative you're in landscape (landscape left)
+                *
+                * Similarly, you can use the pitch to differentiate between portrait and reverse portrait.
+                * If the pitch is positive, you're in reverse portrait, and if the pitch is negative you're in portrait.
+                *
+                * orientation -> azimut, pitch and roll
+                *
+                *
+                *
+                */
+
+                /*
+                 * values[0]  表示Z轴的角度：方向角，我们平时判断的东西南北就是看这个数据的，
+                 * 使用第一种方式获得方向（磁场+加速度）得到的数据范围是（-180～180）,也就是说，
+                 * 0表示正北，90表示正东，180/-180表示正南，-90表示正西。
+                 * values[1] pitch 倾斜角  即由静止状态开始，前后翻转，
+                 * 手机顶部往上抬起（0~-90），手机尾部往上抬起（0~90）
+                 * values[2] roll 旋转角 即由静止状态开始，左右翻转，
+                 * 手机左侧抬起（0~90）,手机右侧抬起（0~-90）
+                 */
+
+                azimuth = orientation[0];               // 弧度
+                Timber.d("azimuth" + Math.toDegrees(azimuth));
+                pitch = orientation[1];
+                Timber.d("pitch " + Math.toDegrees(pitch));
+                roll = orientation[2];
+                Timber.d("roll " + Math.toDegrees(roll));
+
+                inclineGravity = gravityValue.clone();
+
+                double norm_Of_g = Math.sqrt(inclineGravity[0] * inclineGravity[0] + inclineGravity[1] * inclineGravity[1] + inclineGravity[2] * inclineGravity[2]);
+
+                // Normalize the accelerometer vector
+                inclineGravity[0] = (float) (inclineGravity[0] / norm_Of_g);
+                inclineGravity[1] = (float) (inclineGravity[1] / norm_Of_g);
+                inclineGravity[2] = (float) (inclineGravity[2] / norm_Of_g);
+
+                //Checks if device is flat on ground or not
+                int inclination = (int) Math.round(Math.toDegrees(Math.acos(inclineGravity[2])));
+
+                /*
+                * Float obj1 = new Float("10.2");
+                * Float obj2 = new Float("10.20");
+                * int retval = obj1.compareTo(obj2);
+                *
+                * if(retval > 0) {
+                * System.out.println("obj1 is greater than obj2");
+                * }
+                * else if(retval < 0) {
+                * System.out.println("obj1 is less than obj2");
+                * }
+                * else {
+                * System.out.println("obj1 is equal to obj2");
+                * }
+                */
+                Float objPitch = new Float(pitch);
+                Float objZero = new Float(0.0);
+                Float objZeroPointTwo = new Float(0.2);
+                Float objZeroPointTwoNegative = new Float(-0.2);
+
+                int objPitchZeroResult = objPitch.compareTo(objZero);
+                int objPitchZeroPointTwoResult = objZeroPointTwo.compareTo(objPitch);
+                int objPitchZeroPointTwoNegativeResult = objPitch.compareTo(objZeroPointTwoNegative);
+
+                if (roll < 0 && ((objPitchZeroResult > 0 && objPitchZeroPointTwoResult > 0) || (objPitchZeroResult < 0 && objPitchZeroPointTwoNegativeResult > 0)) && (inclination > 30 && inclination < 40)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isTiltDownward() {
+        if (gravityValue != null && geomagneticValue != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+
+            boolean success = SensorManager.getRotationMatrix(R, I, gravityValue, geomagneticValue);
+
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+                pitch = orientation[1];
+                roll = orientation[2];
+
+                inclineGravity = gravityValue.clone();
+
+                double norm_Of_g = Math.sqrt(inclineGravity[0] * inclineGravity[0] + inclineGravity[1] * inclineGravity[1] + inclineGravity[2] * inclineGravity[2]);
+
+                // Normalize the accelerometer vector
+                inclineGravity[0] = (float) (inclineGravity[0] / norm_Of_g);
+                inclineGravity[1] = (float) (inclineGravity[1] / norm_Of_g);
+                inclineGravity[2] = (float) (inclineGravity[2] / norm_Of_g);
+
+                //Checks if device is flat on groud or not
+                int inclination = (int) Math.round(Math.toDegrees(Math.acos(inclineGravity[2])));
+
+                Float objPitch = new Float(pitch);
+                Float objZero = new Float(0.0);
+                Float objZeroPointTwo = new Float(0.2);
+                Float objZeroPointTwoNegative = new Float(-0.2);
+
+                int objPitchZeroResult = objPitch.compareTo(objZero);
+                int objPitchZeroPointTwoResult = objZeroPointTwo.compareTo(objPitch);
+                int objPitchZeroPointTwoNegativeResult = objPitch.compareTo(objZeroPointTwoNegative);
+
+                if (roll < 0 && ((objPitchZeroResult > 0 && objPitchZeroPointTwoResult > 0) || (objPitchZeroResult < 0 && objPitchZeroPointTwoNegativeResult > 0)) && (inclination > 140 && inclination < 170)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Queue of samples. Keeps a running average.
+     */
     static class SampleQueue {
 
-        /** Window size in ns. Used to compute the average. */
+        /**
+         * Window size in ns. Used to compute the average.
+         */
         private static final long MAX_WINDOW_SIZE = 500000000; // 0.5s
         private static final long MIN_WINDOW_SIZE = MAX_WINDOW_SIZE >> 1; // 0.25s
 
@@ -129,7 +333,9 @@ public class ScreenFaceDetector implements SensorEventListener {
             }
         }
 
-        /** Removes all samples from this queue. */
+        /**
+         * Removes all samples from this queue.
+         */
         void clear() {
             while (oldest != null) {
                 Sample removed = oldest;
@@ -141,7 +347,9 @@ public class ScreenFaceDetector implements SensorEventListener {
             acceleratingCount = 0;
         }
 
-        /** Purges samples with timestamps older than cutoff. */
+        /**
+         * Purges samples with timestamps older than cutoff.
+         */
         void purge(long cutoff) {
             while (sampleCount >= MIN_QUEUE_SIZE
                     && oldest != null && cutoff - oldest.timestamp > 0) {
@@ -160,7 +368,9 @@ public class ScreenFaceDetector implements SensorEventListener {
             }
         }
 
-        /** Copies the samples into a list, with the oldest entry at index 0. */
+        /**
+         * Copies the samples into a list, with the oldest entry at index 0.
+         */
         List<Sample> asList() {
             List<Sample> list = new ArrayList<Sample>();
             Sample s = oldest;
@@ -172,7 +382,7 @@ public class ScreenFaceDetector implements SensorEventListener {
         }
 
         /**
-         * Returns true if we have enough samples and more than 3/4 of those samples
+         * Returns true if we have enough samples and all of those samples
          * are accelerating.
          */
         boolean isFaceChanging() {
@@ -183,23 +393,35 @@ public class ScreenFaceDetector implements SensorEventListener {
         }
     }
 
-    /** An accelerometer sample. */
+    /**
+     * An accelerometer sample.
+     */
     static class Sample {
-        /** Time sample was taken. */
+        /**
+         * Time sample was taken.
+         */
         long timestamp;
 
-        /** If acceleration > {@link #accelerationThreshold}. */
+        /**
+         * If acceleration > {@link #accelerationThreshold}.
+         */
         boolean accelerating;
 
-        /** Next sample in the queue or pool. */
+        /**
+         * Next sample in the queue or pool.
+         */
         Sample next;
     }
 
-    /** Pools samples. Avoids garbage collection. */
+    /**
+     * Pools samples. Avoids garbage collection.
+     */
     static class SamplePool {
         private Sample head;
 
-        /** Acquires a sample from the pool. */
+        /**
+         * Acquires a sample from the pool.
+         */
         Sample acquire() {
             Sample acquired = head;
             if (acquired == null) {
@@ -211,7 +433,9 @@ public class ScreenFaceDetector implements SensorEventListener {
             return acquired;
         }
 
-        /** Returns a sample to the pool. */
+        /**
+         * Returns a sample to the pool.
+         */
         void release(Sample sample) {
             sample.next = head;
             head = sample;
