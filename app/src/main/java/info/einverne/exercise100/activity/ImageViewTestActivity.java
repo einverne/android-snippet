0e4c637c7a424c9030c74985ee8e5f33cf7af323
wujiawei <jiawei.wu@ihandysoft.com>
2016-09-01 13:01:15 +0800

    Add ImageView ScaleType test

---
+++ app/src/main/java/info/einverne/exercise100/activity/ImageViewTestActivity.java
@@ -0,0 +1,18 @@
+package info.einverne.exercise100.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.einverne.exercise100.R;
+
+/**
+ * ImageView scaleType test see layout
+ */
+public class ImageViewTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view_test);
+    }
+}
