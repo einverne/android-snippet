758d2e9162f4e77842cdc9189b6172a6b4976b54
wujiawei <jiawei.wu@ihandysoft.com>
2016-09-01 18:57:27 +0800

    Add RoundedDrawable test

--- app/src/main/java/info/einverne/exercise100/activity/RoundImageViewActivity.java
+++ app/src/main/java/info/einverne/exercise100/activity/RoundImageViewActivity.java
@@ -1,15 +1,22 @@
 package info.einverne.exercise100.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.makeramen.roundedimageview.RoundedDrawable;
 import com.makeramen.roundedimageview.RoundedImageView;
 
 import info.einverne.exercise100.R;
+import info.einverne.exercise100.utils.Utils;
 
 public class RoundImageViewActivity extends AppCompatActivity {
 
+    private ImageView mImageView;
     RoundedImageView mRoundedImageView;
 
     @Override
@@ -17,8 +24,13 @@ public class RoundImageViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_image_view);
 
+        mImageView = (ImageView) findViewById(R.id.imageView);
         mRoundedImageView = (RoundedImageView) findViewById(R.id.roundImageView);
 
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blur_test);
+        RoundedDrawable roundedDrawable = RoundedDrawable.fromBitmap(bitmap);
+        roundedDrawable.setCornerRadius(0, Utils.convertDpToPixel(50f, this), Utils.convertDpToPixel(50f, this), 0);
+        mImageView.setImageDrawable(roundedDrawable);
     }
 
 
