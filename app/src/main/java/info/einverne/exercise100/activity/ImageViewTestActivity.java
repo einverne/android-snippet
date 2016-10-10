5c6004648296cc4004531264322a35297979c5af
wujiawei <jiawei.wu@ihandysoft.com>
2016-10-10 15:13:47 +0800

    ImageView ScaleType test

--- app/src/main/java/info/einverne/exercise100/activity/ImageViewTestActivity.java
+++ app/src/main/java/info/einverne/exercise100/activity/ImageViewTestActivity.java
@@ -1,18 +1,34 @@
 package info.einverne.exercise100.activity;
 
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import info.einverne.exercise100.R;
 
 /**
- * ImageView scaleType test see layout
+ * ImageView scaleType test, check layout for more details
  */
 public class ImageViewTestActivity extends AppCompatActivity {
 
+    private ImageView ivMatrix;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_view_test);
+        setContentView(R.layout.activity_image_view_matrix);
+        ivMatrix = (ImageView) findViewById(R.id.ivMatrix);
+
+        float image_width = ivMatrix.getWidth();
+        float image_height = ivMatrix.getHeight();
+        float screen_width = getWindowManager().getDefaultDisplay().getWidth();
+        float screen_height = getWindowManager().getDefaultDisplay().getHeight();
+        RectF drawableRect = new RectF(0, 0, image_width, image_height);
+        RectF viewRect = new RectF(0, 0, screen_width, screen_height);
+
+        Matrix matrix = new Matrix();
+        matrix.postTranslate(screen_width/2, screen_height/2);
+        ivMatrix.setImageMatrix(matrix);
     }
 }
