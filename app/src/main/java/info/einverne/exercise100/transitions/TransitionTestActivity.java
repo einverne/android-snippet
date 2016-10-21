fe6e6bbb111ad56a4e9af3e25b3596a668b904db
wujiawei <jiawei.wu@ihandysoft.com>
2016-10-21 15:57:32 +0800

    Add transition test

---
+++ app/src/main/java/info/einverne/exercise100/transitions/TransitionTestActivity.java
@@ -0,0 +1,15 @@
+package info.einverne.exercise100.transitions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.einverne.exercise100.R;
+
+public class TransitionTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_test);
+    }
+}
